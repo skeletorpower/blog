98bb8781d69c3e6c6dc979e16fe89808381b5578
skeletorpower <aleksandarnovic@gmail.com>
2017-12-25 13:39:18 +0100

    Slug problem commit

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+
+# from django.autoslug import AutoSlugField
 
 from django.urls import reverse
 
@@ -14,6 +18,8 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    # slug = models.SlugField()
+    # slug = AutoSlugField(populate_from='title', unique_with='pub_date__month')
     image = models.ImageField(upload_to=upload_location,
                               null=True, blank=True,
                               width_field='width_field',
@@ -39,4 +45,26 @@ class Post(models.Model):
         ordering = ["-timestamp", "-updated"]
 
 
+# def create_slug(instance, new_slug=None):
+#     slug = slugify(instance.title)
+#     if new_slug is not None:
+#         slug=new_slug
+#     qs = Post.objects.filter(slug=slug).order_by("-id")
+#
+#     exists = qs.exists()
+#     if exists:
+#         new_slug = "%s-%s" %(slug, qs.first().id)
+#         return create_slug(instance, new_slug=new_slug)
+#     return slug
+#
+#
+#
+#
+# def pre_save_post_receiver(sender, instance, *args, **kwargs):
+#     if not instance.slug:
+#         instance.slug = create_slug(instance)
+#
+#
+#
+# pre_save.connect(pre_save_post_receiver, sender=Post)
 
